10a460a745f203c9176a561480c4ae85bd6f1286
Toren2313 <przemo_653@poczta.fm>
2025-02-18 00:06:41 +0100

    feat: :sparkles: added loader

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,42 @@ import MainPage from "./pages/main.page";
 import { Navbar } from "./components/navbar";
 import AboutPage from "./pages/about.page";
 import { IParallax, Parallax, ParallaxLayer } from "@react-spring/parallax";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+import Loader from "./components/loader";
 
 export default function App() {
   const ref = useRef<IParallax>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
-  return (
-    <BrowserRouter>
-      <Navbar parallaxRef={ref} />
-      <Parallax ref={ref} pages={2} style={{ top: 0, left: 0 }}>
-        <ParallaxLayer offset={0} speed={2.5}>
-          <MainPage parallaxRef={ref} />
-        </ParallaxLayer>
-        <ParallaxLayer offset={1} speed={2.5}>
-          <AboutPage />
-        </ParallaxLayer>
-      </Parallax>
-    </BrowserRouter>
-  );
+  const handleLoading = () => {
+    setIsLoading((prevState) => !prevState);
+  };
+
+  useEffect(() => {
+    window.addEventListener("load", Loader);
+
+    return () => window.removeEventListener("load", handleLoading);
+  }, []);
+
+  setTimeout(() => {
+    setIsLoading(false);
+  }, 2500);
+
+  const returnApp = () => {
+    return (
+      <BrowserRouter>
+        <Navbar parallaxRef={ref} />
+        <Parallax ref={ref} pages={2} style={{ top: 0, left: 0 }}>
+          <ParallaxLayer offset={0} speed={2.5}>
+            <MainPage parallaxRef={ref} />
+          </ParallaxLayer>
+          <ParallaxLayer offset={1} speed={2.5}>
+            <AboutPage />
+          </ParallaxLayer>
+        </Parallax>
+      </BrowserRouter>
+    );
+  };
+
+  return isLoading ? <Loader /> : returnApp();
 }
